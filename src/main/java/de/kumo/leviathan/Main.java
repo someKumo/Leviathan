aaa97a1443b7a7370d897ec15bd5a6a31183927b
Kumo <danielkuehnel01@gmail.com>
2022-07-21 21:15:24 +0200

    update

--- src/main/java/de/kumo/leviathan/Main.java
+++ src/main/java/de/kumo/leviathan/Main.java
@@ -2,6 +2,7 @@ package de.kumo.leviathan;
 
 import de.kumo.leviathan.commands.*;
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import java.awt.*;
@@ -14,10 +15,21 @@ public class Main extends JavaPlugin implements Listener {
     public static String pre = "§8[§bLeviathan§8] | §7";
     public static String noperm = "§8Sorry. not today!";
 
+    FileConfiguration config = getConfig();
+
     public static ArrayList<String> mute = new ArrayList<>();
     public static ArrayList<String> live = new ArrayList<>();
 
     public void onEnable() {
+        config.addDefault("is the Config working? (should be true)", true);
+        config.options().copyDefaults(true);
+        saveConfig();
+
+        if(config.getBoolean("is the Config working? (should be true)")) {
+            Bukkit.getConsoleSender().sendMessage(Main.pre + Color.darkGray + "Config is fine!");
+        } else {
+            Bukkit.getConsoleSender().sendMessage(Main.pre + Color.darkGray + "Config is " + Color.red+ "not" + Color.darkGray + "fine!");
+        }
         Bukkit.getConsoleSender().sendMessage(Main.pre + Color.green + "successfully started up!");
 
 
