77c44e7ce3bcda55fb8056041fe953dea36c3e59
someKumo <simplykumo@gmail.com>
2022-07-21 14:35:05 +0200

    created config?

--- src/main/java/de/kumo/leviathan/Main.java
+++ src/main/java/de/kumo/leviathan/Main.java
@@ -2,15 +2,19 @@ package de.kumo.leviathan;
 
 import de.kumo.leviathan.commands.*;
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import java.util.ArrayList;
 import java.util.Objects;
 
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin implements Listener {
 
     public static String pre = "§8[§bLeviathan§8] | §7";
     public static String noperm = "§8Sorry. not today";
+
+    FileConfiguration config = getConfig();
 
     public static ArrayList<String> mute = new ArrayList<>();
     public static ArrayList<String> live = new ArrayList<>();
@@ -19,12 +23,11 @@ public class Main extends JavaPlugin {
 
 
     public void onEnable() {
-        Bukkit.getConsoleSender().sendMessage(Main.pre + "§8booting...");
-        Bukkit.getConsoleSender().sendMessage(Main.pre + "§8loading Libraries...");
-        Bukkit.getConsoleSender().sendMessage(Main.pre + "§8loading Commands...");
-        Bukkit.getConsoleSender().sendMessage(Main.pre + "§8loading Essentials...");
-        Bukkit.getConsoleSender().sendMessage(Main.pre + "§8booting §2finished!");
         Bukkit.getConsoleSender().sendMessage(Main.pre + "§8Leviathan started up");
+
+        config.addDefault("is the Config working? (should be true)", true);
+        config.options().copyDefaults(true);
+        saveConfig();
 
         //Command Registration
 
@@ -33,11 +36,7 @@ public class Main extends JavaPlugin {
         Objects.requireNonNull(getCommand("mute")).setExecutor(new mute());
         Objects.requireNonNull(getCommand("workbench")).setExecutor(new workbench());
         Objects.requireNonNull(getCommand("live")).setExecutor(new streaming());
-
-
-
-
-
+        Objects.requireNonNull(getCommand("inventory")).setExecutor((new inventory()));
     }
 
     public void onDisable() {
